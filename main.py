c49b7505974299820326ade9909ec2369638ccf4
Adrianne Gore <adrigore89@gmail.com>
2017-10-04 08:53:05 -0500

    got a couple of errors to show

--- main.py
+++ main.py
@@ -5,19 +5,35 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True  
 
-@app.route("/" methods=['POST'])
+@app.route("/")
 def index():
-
-    #if all the info is correct,
-    #redirect ("/welcome.html")
 
     return render_template('index.html')
 
-@app.route("/welcome")
-def welcome():
+@app.route("/", methods=['POST'])
+def validate_info():
+    username= request.form['username']
+    password= request.form['password']
+    verify= request.form['verify']
+    password= request.form['password']
+
+    username_error=''
+    password_error=''
+
+    if username =='':
+        username_error='Please enter a username (3-20 charcters)'
     
-    #add tag/placeholder for username
+    if password != verify:
+        password_error="Passwords do not match!"
 
-    return render_template("welcome.html")
+    return render_template('index.html', username_error=username_error, password_error=password_error)
 
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    name= request.form['username']
+    #DONE! tag/placeholder for username
+
+    return render_template("welcome.html", name=name)
+#redirect example: take to a whole new page
+#render example: reload the same template with error info 
 app.run()
