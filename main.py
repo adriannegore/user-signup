677686ff127276d2c2e9852c06665ed9195208aa
Adrianne Gore <adrigore89@gmail.com>
2017-10-04 11:23:13 -0500

    added all errors, formatted welcome.html with table

--- main.py
+++ main.py
@@ -15,25 +15,54 @@ def validate_info():
     username= request.form['username']
     password= request.form['password']
     verify= request.form['verify']
-    password= request.form['password']
+    email= request.form['email']
 
     username_error=''
     password_error=''
+    verify_error=''
+    email_error=''
 
     if username =='':
         username_error='Please enter a username (3-20 charcters)'
+    elif not 3 <= len(username) <= 20:
+        username_error='Username must be 3-20 characters long.'
+    elif ' ' in username:
+        username_error='No spaces allowed.'
+
     
-    if password != verify:
-        password_error="Passwords do not match!"
+    if password=='':
+        password_error='Please enter a password (3-20 characters)'
+    elif not 3 <= len(password) <= 20:
+        password_error='Password must be 3-20 characters long.'
+    elif ' ' in password:
+        password_error='No spaces allowed.'
+    
+    if verify=='':
+        verify_error='Please verify your password'
+    elif password != verify:
+        verify_error="Passwords do not match!"
+    
+    if not email=='':
+        if not 3 <= len(email) <= 20:
+            email_error='Email must be 3-20 characters long'
+        elif not email.count('@')== 1 or not email.count('.') == 1 or ' ' in email:
+            email_error="Please enter a valid email"
 
-    return render_template('index.html', username_error=username_error, password_error=password_error)
+    if not username_error and not password_error and not verify_error and not email_error:
+        return render_template('welcome.html', username=username)
+    else:
+        return render_template('index.html', 
+            username_error=username_error, password_error=password_error, 
+            verify_error=verify_error, email_error=email_error, 
+            username=username, email=email)
 
-@app.route("/welcome", methods=['POST'])
-def welcome():
-    name= request.form['username']
-    #DONE! tag/placeholder for username
-
-    return render_template("welcome.html", name=name)
+### I don't really need the app route 
+# because no one would ever need to go directly to my welcome page.
+#The welcome is just some html to display###
+#@app.route("/welcome")
+#def welcome():
+    #username=request.args.get('username')
+    return render_template("welcome.html", username=username)
 #redirect example: take to a whole new page
 #render example: reload the same template with error info 
 app.run()
